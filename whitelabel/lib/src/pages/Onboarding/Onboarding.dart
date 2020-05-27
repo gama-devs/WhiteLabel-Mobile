a6d544c9e70ac3a0b08798442f8dd3895784e242
jonjon667 <jonjon667@hotmail.com>
2020-05-27 06:37:17 -0300

    Linkando botão de próximo ao carousel

--- whitelabel/lib/src/pages/Onboarding/Onboarding.dart
+++ whitelabel/lib/src/pages/Onboarding/Onboarding.dart
@@ -4,14 +4,13 @@ import 'package:flutter/services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
 
+
 class Onboarding extends StatefulWidget {
   @override
   _OnboardingState createState() => new _OnboardingState();
 }
-
 class _OnboardingState extends State<Onboarding> {
-  var indexCarousel = 0; 
-  PageController pageController;                                     
+  var indexCarousel = 0;                                    
   Size displaySize(BuildContext context) {
     return MediaQuery.of(context).size;
   }
@@ -23,7 +22,8 @@ class _OnboardingState extends State<Onboarding> {
     return displaySize(context).width;
   }
 
- 
+  CarouselController buttonCarouselController = CarouselController();
+  
   @override
   Widget build(BuildContext context) {
   final List <String> image = ['assets/pizza.png','assets/entregadorpizza.png','assets/mulhercelular.png'];
@@ -34,47 +34,77 @@ class _OnboardingState extends State<Onboarding> {
   final List <String> textGrey = ['Um aplicativo para você aproveitar ao máximo o sabor e qualidade das nossas pizzas.',
   'Escolha quando e onde quer receber nossa pizza e aproveite esse momento.',
   'Um aplicativo para você aproveitar ao máximo o sabor e qualidade das nossas pizzas.'];
+  final List <String> buttonText = ["Próximo  >","Próximo  >","Ir para pizzas :D"];
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    final nextButon = Material(
-        elevation: 5.0,
-        borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFFFF805D),
-        child: MaterialButton(
-          minWidth: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 15.0),
-          onPressed: (){
-            print(textBold[indexCarousel]);
-          },
-          child: Text("Próximo  >",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontFamily: 'Roboto',color: Colors.white),
-        ),
-      )
-    );
-
 
     CarouselSlider carousel = CarouselSlider(
-            options: CarouselOptions(
-              height: 400,
-              viewportFraction: 1,
-              onPageChanged: (index,reason){
-                print(index);
-                setState(() {
-                  indexCarousel = index;
-                });
-                print(indexCarousel);
-              }
-            ),
-            items: image.map((item) => Container(
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(item, fit: BoxFit.cover,height: 400 ,width: MediaQuery.of(context).size.width),
-                  ],
-                ) 
-              ),
-            )).toList(),
+      carouselController: buttonCarouselController,
+      options: CarouselOptions(
+        height: 450,
+        viewportFraction: 1,
+        onPageChanged: (index,reason){
+          print(index);
+          setState(() {
+            indexCarousel = index;
+          });
+          print(indexCarousel);
+        }
+      ),
+      items: image.map((item) => Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Image.asset(item, fit: BoxFit.cover,height: 410 ,width: MediaQuery.of(context).size.width),
+            ],
+          ) 
+        ),
+      )).toList(),
     );
+
+
+    checkBox(position,index){
+      if(position == index){
+        return SizedBox(
+          height: 10,
+          width: 10,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0) ,
+              color: Color(0xFFFF805D)
+              ),
+            ),
+          );
+      }
+      return SizedBox(
+        height: 7,
+        width: 7,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0) ,
+            color: Color(0xFFE5E5E5)
+            ),
+          ),
+        );
+    };
+
+    final nextButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Color(0xFFFF805D),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 15.0),
+        onPressed: (){
+          if(indexCarousel != 2) return buttonCarouselController.nextPage();
+
+          else print(textBold[indexCarousel]);
+        },
+        child: Text(buttonText[indexCarousel],
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: 'Roboto',color: Colors.white),
+      ),
+    )
+  );
 
     Container carouselText = Container(
       child:Column(
@@ -108,12 +138,25 @@ class _OnboardingState extends State<Onboarding> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  IconButton(
+                  Stack(children: <Widget>[
+                    IconButton(
                     onPressed: (){},
                     icon:Icon(Icons.close)),
                   carousel,
+                  ],),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      checkBox(0, indexCarousel),
+                      SizedBox(width: 1,),
+                      checkBox(1, indexCarousel),
+                      SizedBox(width: 1,),
+                      checkBox(2, indexCarousel)
+                    ],),
+                  SizedBox(height: 20,),
                   carouselText,
-                  nextButon
+                  nextButton,
               ])),
         )
       ]),
