5df9ad7098e4b91dbcd84421351853926037baf2
Wictorgirardi <wictor.girardi@gmail.com>
2020-05-28 19:26:01 -0300

    animaçoes na tela de cpf

--- whitelabel/lib/src/pages/CEP/cep.dart
+++ whitelabel/lib/src/pages/CEP/cep.dart
@@ -22,33 +22,7 @@ class _CepState extends State<Cep> {
     return displaySize(context).width;
   }
 
-  bool isSwitched = true;
-
-  Widget Logo() {
-    return (Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: displayWidth(context) * 0.8,
-          child: Image.asset('assets/log.png'),
-        ),
-      ],
-    ));
-  }
-
-  Widget Photo() {
-    return (Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: displayWidth(context) * 0.55,
-          child: Image.asset('assets/hand.png'),
-        ),
-      ],
-    ));
-  }
+  bool isSwitched = true, isValid = false, isInvalid = false;
 
   Widget Descricao() {
     return (Row(
@@ -59,7 +33,7 @@ class _CepState extends State<Cep> {
         Container(
           width: displayWidth(context) * 0.8,
           child: Text(
-            'Precisamos verificar se atendemos sua região.',
+            'Precisamos verificar\n se atendemos sua região.',
             textAlign: TextAlign.center,
             style: new TextStyle(
               fontSize: 23.0,
@@ -77,7 +51,7 @@ class _CepState extends State<Cep> {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Padding(padding: EdgeInsets.only(top: 40.0, bottom: 40)),
+        Padding(padding: EdgeInsets.only(top: 40.0)),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -85,10 +59,10 @@ class _CepState extends State<Cep> {
             Container(
               width: displayWidth(context) * 0.15,
               child: Switch(
-                value: isSwitched,
+                value: isValid,
                 onChanged: (value) {
                   setState(() {
-                    isSwitched = value;
+                  isValid = !isValid;
                   });
                 },
                 activeTrackColor: Colors.orange,
@@ -121,95 +95,124 @@ class _CepState extends State<Cep> {
   }
 
   Widget Card() {
-    return (Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Padding(padding: EdgeInsets.only(top: 200.0)),
-        Container(
-          decoration: BoxDecoration(
-              color: Color(0xFFFF805D),
-              border: Border.all(
-                color: Color(0xFFFF805D),
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          width: displayWidth(context),
-          height: displayHeight(context) * 0.3,
-          child: Column(
-            children: <Widget>[
-              Padding(padding: EdgeInsets.only(top: 33.0)),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+    return (Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.bounceInOut,
+              decoration: BoxDecoration(
+                  color: isValid ? Color(0xFF1BD09A) : Color(0xFFFF805D),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              width: displayWidth(context),
+              height: displayHeight(context) * 0.3,
+              child: Column(
                 children: <Widget>[
-                  Text(
-                    'Informe o CEP de entrega.',
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(
-                      fontSize: 19.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(padding: EdgeInsets.only(top: 33.0)),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
+                  Padding(padding: EdgeInsets.only(top: 33.0)),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: displayWidth(context) * 0.7,
+                        child: Text(
+                          isValid
+                              ? 'Maravilha!\n Nós atendemos sua região'
+                              : 'Informe o CEP de entrega.',
+                          textAlign: TextAlign.center,
+                          style: new TextStyle(
+                            fontSize: 19.0,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                      width: displayWidth(context) * 0.9,
-                      height: displayHeight(context) * 0.09,
-                      child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(
-                                  top: displayHeight(context) * 0.03,
-                                  left: displayWidth(context) * 0.06),
-                              child: Text(
-                                'Seu CEP',
-                                textAlign: TextAlign.center,
-                                style: new TextStyle(
-                                  fontSize: 19.0,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.black,
-                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 33.0)),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      AnimatedContainer(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.ease,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.white,
                               ),
-                            ),
-                            Padding(
-                                padding: EdgeInsets.only(
-                                    top: displayHeight(context) * 0.01,
-                                    bottom: displayHeight(context) * 0.01,
-                                    left: displayWidth(context) * 0.45),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Color(0xFF413131),
-                                      border: Border.all(
-                                        color: Color(0xFF413131),
-                                      ),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(20))),
-                                  width: displayWidth(context) * 0.15,
-                                  height: displayHeight(context) * 0.09,
-                                  child: Icon(Icons.check, color: Colors.white),
-                                )),
-                          ]))
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          width: isValid
+                              ? displayWidth(context) * 0.2
+                              : displayWidth(context) * 0.9,
+                          height: displayHeight(context) * 0.09,
+                          child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      top: isValid
+                                          ? displayHeight(context) * 0.02
+                                          : displayHeight(context) * 0.03,
+                                      left: displayWidth(context) * 0.06),
+                                  child: Text(
+                                    isValid ? '🛵' : 'Seu CEP',
+                                    textAlign: TextAlign.center,
+                                    style: new TextStyle(
+                                      fontSize: isValid ? 26 : 19,
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      top: isValid
+                                          ? 0
+                                          : displayHeight(context) * 0.01,
+                                      bottom: isValid
+                                          ? 0
+                                          : displayHeight(context) * 0.01,
+                                      left: isValid
+                                          ? 0
+                                          : displayWidth(context) * 0.45),
+                                  child: isValid
+                                      ? null
+                                      : new GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              isValid = !isValid;
+                                            });
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Color(0xFF413131),
+                                                border: Border.all(
+                                                  color: Color(0xFF413131),
+                                                ),
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(20))),
+                                            width: displayWidth(context) * 0.15,
+                                            height:
+                                                displayHeight(context) * 0.09,
+                                            child: Icon(Icons.check,
+                                                color: Colors.white),
+                                          )),
+                                ),
+                              ]))
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        ),
-      ],
-    ));
+              ),
+            ),
+          ],
+        )));
   }
 
   @override
@@ -219,13 +222,22 @@ class _CepState extends State<Cep> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: new Stack(fit: StackFit.expand, children: <Widget>[
+        Positioned.fill(
+          child: Card(),
+        ),
         Container(
             child: Column(children: <Widget>[
-          Logo(),
-          Photo(),
+          Padding(padding: EdgeInsets.only(top: 40.0)),
+           SvgPicture.asset(
+            'assets/Logo.svg',
+            width: displayWidth(context) * 0.8,
+            height: displayHeight(context) * 0.1,
+          ),
+          Padding(
+              padding: EdgeInsets.only(
+                  top: displayHeight(context) * 0.3, bottom: 40)),
           Descricao(),
           Toogle(),
-          Card(),
         ])),
       ]),
     );
