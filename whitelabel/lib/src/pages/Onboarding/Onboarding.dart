715afa18ab371e08b5049eb3704c4fc50b390fad
Wictorgirardi <wictor.girardi@gmail.com>
2020-06-01 01:49:20 -0300

    melhorias na tela de onboarding

--- whitelabel/lib/src/pages/Onboarding/Onboarding.dart
+++ whitelabel/lib/src/pages/Onboarding/Onboarding.dart
@@ -28,26 +28,18 @@ class _OnboardingState extends State<Onboarding> {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> image = [
-      'assets/pizza.png',
-      'assets/entregadorpizza.png',
-      'assets/mulhercelular.png'
-    ];
+    final List<String> image = ['assets/3.png', 'assets/2.png', 'assets/1.png'];
     final List<String> textBold = [
-      'Sua pizza favorita onde sempre deveria estar, na palma da sua mão.',
+      'Sua pizza favorita\n onde sempre deveria estar,\n na palma da sua mão.',
       'Receba nossa pizza quentinha no conforto da sua casa.',
-      'Compre e ganhe pontos para trocar por produtos.'
+      'Compre e ganhe pontos\n para trocar por produtos.'
     ];
     final List<String> textGrey = [
-      'Um aplicativo para você aproveitar ao máximo o sabor e qualidade das nossas pizzas.',
-      'Escolha quando e onde quer receber nossa pizza e aproveite esse momento.',
-      'Um aplicativo para você aproveitar ao máximo o sabor e qualidade das nossas pizzas.'
+      'Um aplicativo para você aproveitar\n ao máximo o sabor e qualidade\n das nossas pizzas.',
+      'Escolha quando e onde quer\n receber nossa pizza e aproveite\n esse momento.',
+      'Um aplicativo para você aproveitar\n ao máximo o sabor e qualidade\n das nossas pizzas.'
     ];
-    final List<String> buttonText = [
-      "Próximo  >",
-      "Próximo  >",
-      "Ir para pizzas :D"
-    ];
+
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
     CarouselSlider carousel = CarouselSlider(
@@ -66,7 +58,8 @@ class _OnboardingState extends State<Onboarding> {
           .map((item) => Container(
                 child: Center(
                     child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
                         height: 350,
@@ -107,102 +100,144 @@ class _OnboardingState extends State<Onboarding> {
     ;
 
     final nextButton = Material(
-        elevation: 5.0,
-        borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFFFF805D),
-        child: MaterialButton(
-          minWidth: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 15.0),
-          onPressed: () {
-            if (indexCarousel != 2)
-              return buttonCarouselController.nextPage();
-            else
-               Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => Cep()
-        ));
+      borderRadius: BorderRadius.circular(10.0),
+      color: Color(0xFFFF805D),
+      child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 15.0),
+            onPressed: () {
+              if (indexCarousel != 2)
+                return buttonCarouselController.nextPage();
+              else
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => Cep()));
               print(textBold[indexCarousel]);
-          },
-          child: Text(
-            buttonText[indexCarousel],
-            textAlign: TextAlign.center,
-            style: TextStyle(fontFamily: 'Roboto', color: Colors.white),
-          ),
-        ));
+            },
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  indexCarousel == 0 || indexCarousel == 1
+                      ? 'Próximo'
+                      : 'Ir para pizzas',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontFamily: 'Roboto', color: Colors.white),
+                ),
+                indexCarousel == 0 || indexCarousel == 1
+                    ? Container(
+                        width: 30,
+                        height: 30,
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 18,
+                          color: Colors.white,
+                        ))
+                    : Container(
+                        width: 30,
+                        height: 30,
+                        child: Text(
+                          '🤤',
+                          textAlign: TextAlign.center,
+                          style: new TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                        )),
+              ],
+            ),
+          )),
+    );
 
     Container carouselText = Container(
-        child: Column(children: <Widget>[
-      Text(
-        textBold[indexCarousel],
-        style: TextStyle(
-            color: Color(0xFF413131),
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            wordSpacing: 1),
-        textAlign: TextAlign.center,
-      ),
-      SizedBox(
-        height: 30,
-      ),
-      Text(
-        textGrey[indexCarousel],
-        style:
-            TextStyle(color: Color(0xFF868484), fontSize: 15, wordSpacing: 1),
-        textAlign: TextAlign.center,
-      ),
-      SizedBox(
-        height: 30,
-      ),
-    ]));
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Text(
+            textBold[indexCarousel],
+            style: TextStyle(
+                color: Color(0xFF413131),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                wordSpacing: 1),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            textGrey[indexCarousel],
+            style: TextStyle(
+                color: Color(0xFF868484), fontSize: 16, wordSpacing: 1),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ]));
 
     return Scaffold(
       body: new Stack(fit: StackFit.expand, children: <Widget>[
-        SingleChildScrollView(
-          child: Container(
-              padding: EdgeInsets.all(5.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Stack(
-                      children: <Widget>[
-                        new GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacement(context,
-                                MaterialPageRoute(builder: (context) => Cep()));
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: IconButton(
-                                onPressed: () {}, icon: Icon(Icons.close)),
-                          ),
+        new Positioned(
+          child: new Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                  padding: EdgeInsets.only(bottom: 70.0, left: 30, right: 30),
+                  child: nextButton)),
+        ),
+        Container(
+            padding: EdgeInsets.all(0.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      carousel,
+                      new GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => Cep()));
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 40),
+                          child: IconButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Cep()));
+                              },
+                              icon: Icon(Icons.close)),
                         ),
-                        carousel,
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        checkBox(0, indexCarousel),
-                        SizedBox(
-                          width: 1,
-                        ),
-                        checkBox(1, indexCarousel),
-                        SizedBox(
-                          width: 1,
-                        ),
-                        checkBox(2, indexCarousel)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    carouselText,
-                    nextButton,
-                  ])),
-        )
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      checkBox(0, indexCarousel),
+                      SizedBox(
+                        width: 1,
+                      ),
+                      checkBox(1, indexCarousel),
+                      SizedBox(
+                        width: 1,
+                      ),
+                      checkBox(2, indexCarousel)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(child: carouselText)
+                ])),
       ]),
     );
   }
