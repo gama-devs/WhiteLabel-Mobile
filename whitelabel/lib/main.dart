3d4e7a51807d0508aa48a7e9df5f8237dced3056
jonjon667 <jonjon667@hotmail.com>
2020-06-16 20:50:05 -0300

    Realizando a lógica da tela de produto

--- whitelabel/lib/main.dart
+++ whitelabel/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:whitelabel/src/pages/CEP/cep.dart';
+import 'package:whitelabel/src/pages/Login/login.dart';
 import 'package:whitelabel/src/pages/Onboarding/Onboarding.dart';
 import 'package:whitelabel/src/pages/Menu/menu.dart';
 import 'package:whitelabel/src/pages/Password/password.dart';
@@ -12,5 +13,5 @@ import 'src/pages/Splash/splash.dart';
 void main() {
   runApp(MaterialApp(
 /*      home: Cep() */
-      home: Splash() ));
+      home: Login() ));
 }
--- whitelabel/lib/src/pages/Product/product.dart
+++ whitelabel/lib/src/pages/Product/product.dart
@@ -9,15 +9,35 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Options {
-  int selectedOption;
-  List<String> options;
-  Options({this.selectedOption,options});
+class OptionCategory {
+  int selected;
+  String name;
+  int min;
+  int max;
+  int nOptionsSelected;
+  int required;
+  List<Option> options;
+
+  OptionCategory(
+      {this.name,
+      this.min,
+      this.max,
+      this.required,
+      this.selected = -1,
+      this.options,
+      this.nOptionsSelected = 0});
+}
+
+class Option {
+  String name;
+  String description;
+  double price;
+  Option({this.name, this.description, this.price});
 }
 
 class Product extends StatefulWidget {
-    final Produto selectedProduct;
-   Product({Key key, @required this.selectedProduct}) : super(key: key);
+  final Produto selectedProduct;
+  Product({Key key, @required this.selectedProduct}) : super(key: key);
   @override
   _ProductState createState() => new _ProductState();
 }
@@ -25,11 +45,53 @@ class Product extends StatefulWidget {
 class _ProductState extends State<Product> {
   //Abrindo o bottomSheet ao iniciar a tela
   var tolken;
-
+  List<OptionCategory> selectedOptions = [];
   String searchString = "";
-
+  List<OptionCategory> listCategories = [];
   @override
-  void initState() {}
+  void initState() {
+    List<OptionCategory> optionCategories = [];
+    for (int i = 0;
+        i < widget.selectedProduct.jsonData['option_categories'].length;
+        i++) {
+      List<Option> optionList = [];
+      print("Option");
+      print(widget.selectedProduct.jsonData['option_categories'][i]);
+      for (int j = 0;
+          j <
+              widget.selectedProduct.jsonData['option_categories'][i]['options']
+                  .length;
+          j++) {
+        optionList.add(Option(
+            name: widget.selectedProduct.jsonData['option_categories'][i]
+                ['options'][j]['name'],
+            description: widget.selectedProduct
+                .jsonData['option_categories'][i]['options'][j]['description']
+                .toString(),
+            price: widget.selectedProduct
+                .jsonData['option_categories'][i]['options'][j]['price']
+                .toDouble()));
+      }
+      selectedOptions.add(OptionCategory(
+          options: [],
+          name: widget.selectedProduct.jsonData['option_categories'][i]['name'],
+          max: widget.selectedProduct.jsonData['option_categories'][i]['max'],
+          min: widget.selectedProduct.jsonData['option_categories'][i]['min'],
+          required: widget.selectedProduct.jsonData['option_categories'][i]
+              ['required']));
+      optionCategories.add(OptionCategory(
+          options: optionList,
+          name: widget.selectedProduct.jsonData['option_categories'][i]['name'],
+          max: widget.selectedProduct.jsonData['option_categories'][i]['max'],
+          min: widget.selectedProduct.jsonData['option_categories'][i]['min'],
+          required: widget.selectedProduct.jsonData['option_categories'][i]
+              ['required']));
+    }
+    setState(() {
+      listCategories = optionCategories;
+    });
+    super.initState();
+  }
 
   Size displaySize(BuildContext context) {
     return MediaQuery.of(context).size;
@@ -46,43 +108,47 @@ class _ProductState extends State<Product> {
   Widget build(BuildContext context) {
     Produto product = widget.selectedProduct;
     print(product.jsonData);
-    Padding topBar (name) { return Padding(
-        padding: EdgeInsets.only(top: 50, bottom: 0),
-        child: Container(
-            color: Color(0xFFF8F6F8),
-            height: displayHeight(context) * 0.13,
-            width: displayWidth(context) * 1,
-            child: Row(children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 0, left: 20),
-                child: Container(
-                    width: displayWidth(context) * 0.13,
-                    height: displayHeight(context) * 0.06,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                    child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(context,
-                              MaterialPageRoute(builder: (context) => Menu()));
-                        },
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Color(0xFFFF805D),
-                          size: 20,
-                        ))),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 0, left: 75),
-                child: Container(
-                    child: Text(name,
-                        style: TextStyle(
-                            color: Colors.orange,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w800))),
-              ),
-            ])));}
+    Padding topBar(name) {
+      return Padding(
+          padding: EdgeInsets.only(top: 50, bottom: 0),
+          child: Container(
+              color: Color(0xFFF8F6F8),
+              height: displayHeight(context) * 0.13,
+              width: displayWidth(context) * 1,
+              child: Row(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 0, left: 20),
+                  child: Container(
+                      width: displayWidth(context) * 0.13,
+                      height: displayHeight(context) * 0.06,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Menu()));
+                          },
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            color: Color(0xFFFF805D),
+                            size: 20,
+                          ))),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 0, left: 75),
+                  child: Container(
+                      child: Text(name,
+                          style: TextStyle(
+                              color: Colors.orange,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w800))),
+                ),
+              ])));
+    }
 
     Padding details = Padding(
       padding: EdgeInsets.only(top: 0, left: 0),
@@ -110,10 +176,11 @@ class _ProductState extends State<Product> {
                     width: displayWidth(context) * 0.5,
                     height: displayHeight(context) * 0.03,
                     color: Colors.transparent,
-                    child: Text("R\$: " +
-                                        (product.price / 100)
-                                            .toStringAsFixed(2)
-                                            .replaceAll('.', ','),
+                    child: Text(
+                        "R\$: " +
+                            (product.price / 100)
+                                .toStringAsFixed(2)
+                                .replaceAll('.', ','),
                         style: TextStyle(
                             color: Color(0xFF413131),
                             fontSize: 17,
@@ -124,7 +191,10 @@ class _ProductState extends State<Product> {
                         width: displayWidth(context) * 0.5,
                         height: displayHeight(context) * 0.05,
                         color: Colors.transparent,
-                        child: Text(product.description == null? "Descrição do produto" : product.description,
+                        child: Text(
+                            product.description == null
+                                ? "Descrição do produto"
+                                : product.description,
                             style: TextStyle(
                                 color: Color(0xFF413131),
                                 fontSize: 14,
@@ -162,43 +232,208 @@ class _ProductState extends State<Product> {
       ),
     );
 
-    Container optionsCheckbox(name,index, selectedIndex){
-      return Container(child: Row(children:<Widget>[
-        Text(name),
-        Spacer(),
-        Container(height: 20,
-        width: 20,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          color: index == selectedIndex? Color(0xFFFF805D) : Color(0xFFDEDEDF)),
-        child: index == selectedIndex? Icon(Icons.check):Container(),),
-      ]),);
-    }
-    Container optionsContainer(options) {
-      int selectedIndex = 1;
-      var optionCategory = options['name'];
-      List<Widget> listOptions = [];
-      for(int i = 0; i< options['options'].length;i++){
-        listOptions.add(GestureDetector(
-          onTap: (){
-            setState(() {
-              selectedIndex = i;
-            });
-          },
-          child:optionsCheckbox(options['options'][i]['name'], i, selectedIndex)));
-      }
-      return Container(child: Column(children: <Widget>[
-        Row(children: <Widget>[
-          Text(optionCategory),
+    Container optionsCheckbox(categoryName, option) {
+      return Container(
+        child: Row(children: <Widget>[
+          Text(option.name),
           Spacer(),
-          Container(color: Color(0xFFFF805D),
-          child: Text("1 de 1"),)
-        ],),
-        Column(children: listOptions,)
+          Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                color: selectedOptions[selectedOptions.indexWhere(
+                                (listOption) =>
+                                    listOption.name == categoryName)]
+                            .options
+                            .indexOf(option) !=
+                        -1
+                    ? Color(0xFFFF805D)
+                    : Color(0xFFDEDEDF)),
+            child: selectedOptions[selectedOptions.indexWhere(
+                            (listOption) => listOption.name == categoryName)]
+                        .options
+                        .indexOf(option) !=
+                    -1
+                ? Icon(Icons.check)
+                : Container(),
+          ),
+        ]),
+      );
+    }
+
+    Container optionsCheckBoxContainer(options) {
+      var optionCategory = options.name;
+      List<Widget> listOptions = [];
+      for (int i = 0; i < options.options.length; i++) {
+        if (options.required == 1)
+          listOptions.add(GestureDetector(
+              onTap: () {
+                setState(() {
+                  if (selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .indexOf(options.options[i]) ==
+                      -1) {
+                    if (!(selectedOptions[selectedOptions.indexWhere(
+                                (listOption) =>
+                                    listOption.name == optionCategory)]
+                            .options
+                            .length <
+                        selectedOptions[selectedOptions.indexWhere(
+                                (listOption) =>
+                                    listOption.name == optionCategory)]
+                            .max)) {
+                      selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .removeAt(0);
+                      selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .add(options.options[i]);
+                    } else {
+                      selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .add(options.options[i]);
+                      options.nOptionsSelected += 1;
+                    }
+                  } else {
+                    selectedOptions[selectedOptions.indexWhere(
+                            (listOption) => listOption.name == optionCategory)]
+                        .options
+                        .remove(options.options[i]);
+                    options.nOptionsSelected -= 1;
+                  }
+                });
+              },
+              child: optionsCheckbox(optionCategory, options.options[i])));
+      }
+      return Container(
+          child: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text(optionCategory),
+            Spacer(),
+            Container(
+              color: Color(0xFFFF805D),
+              child: Text(options.nOptionsSelected.toString() +
+                  "de" +
+                  options.max.toString()),
+            )
+          ],
+        ),
+        Column(
+          children: listOptions,
+        )
       ]));
     }
 
+    Container optionsCounter(optionCategory, option) {
+      int numberOfItemsSelected = 0;
+      for (var optionSelected in selectedOptions[selectedOptions
+              .indexWhere((listOption) => listOption.name == optionCategory)]
+          .options) {
+        if (optionSelected == option) numberOfItemsSelected += 1;
+      }
+      return Container(
+        child: Row(children: <Widget>[
+          Text(option.name),
+          Spacer(),
+          Row(
+            children: <Widget>[
+              selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .indexOf(option) !=
+                      -1
+                  ? GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedOptions[selectedOptions.indexWhere(
+                                (listOption) =>
+                                    listOption.name == optionCategory)]
+                            .options
+                            .remove(option);
+                        });
+                        
+                      },
+                      child: Icon(Icons.remove),
+                    )
+                  : Container(),
+              Text(numberOfItemsSelected.toString()),
+              selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .options
+                          .length <
+                      selectedOptions[selectedOptions.indexWhere((listOption) =>
+                              listOption.name == optionCategory)]
+                          .max
+                  ? GestureDetector(
+                      onTap: () {setState(() {
+                        selectedOptions[selectedOptions.indexWhere(
+                                (listOption) =>
+                                    listOption.name == optionCategory)]
+                            .options
+                            .add(option);
+                      });
+                        
+                      },
+                      child: Icon(Icons.add),
+                    )
+                  : Container(),
+            ],
+          )
+        ]),
+      );
+    }
 
+    Container optionsCounterContainer(options) {
+      var optionCategory = options.name;
+      List<Widget> listOptions = [];
+      for (int i = 0; i < options.options.length; i++) {
+          listOptions.add(Container(
+              child: optionsCounter(optionCategory, options.options[i])));
+      }
+      return Container(
+          child: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text(optionCategory),
+            Spacer(),
+            Container(
+              color: Color(0xFFFF805D),
+              child: Text(selectedOptions[selectedOptions
+              .indexWhere((listOption) => listOption.name == optionCategory)]
+          .options.length.toString() +
+                  "de" +
+                  options.max.toString()),
+            )
+          ],
+        ),
+        Column(
+          children: listOptions,
+        )
+      ]));
+    }
+
+    Container requiredOptions(optionList) {
+      List<Widget> listRequiredOptions = [];
+      for (var option in optionList) {
+        if (option.required == 1)
+        listRequiredOptions.add(optionsCheckBoxContainer(option));
+      }
+      for (var option in optionList) {
+        if (option.required != 1)
+        listRequiredOptions.add(optionsCounterContainer(option));
+      }
+      return Container(
+        child: Column(
+          children: listRequiredOptions,
+        ),
+      );
+    }
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -208,7 +443,7 @@ class _ProductState extends State<Product> {
             child: Column(children: <Widget>[
               topBar(product.name),
               details,
-              
+              requiredOptions(listCategories)
             ])),
       ),
     );
